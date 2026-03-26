cdb1bd364b2dca3bd40d27925583db6cdb0ed363
nguyen vinh <dinhvinhh2200@gmail.com>
2026-03-26 21:08:48 +0700

    commit nav

---
+++ src/components/Layout.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Nav from '../layout/Nav';
+
+const Layout = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Layout;
